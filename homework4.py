b00c7fa9c7afdc031a27c420863e5d16e3d7ec81
Zhuravleva0701 <167981362+Zhuravleva0701@users.noreply.github.com>
2024-04-29 22:49:23 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,9 @@
+my_string = input("Введите ваше имя: ")
+print(len(my_string))
+
+print("Введите ваше имя: ".upper())
+print("Введите ваше имя: ".lower())
+print("Введите ваше имя: ".replace(' ', ''))
+my_string = "Введите ваше имя:"
+print(my_string[0])
+print(my_string[-1])
